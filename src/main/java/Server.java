d6341b08af9d88a7847d66673b8326ef29f68319
gustavogogola91 <gustavogogola3@gmail.com>
2026-04-14 22:41:07 -0300

    comentado criação do context

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,7 +6,7 @@ public class Server {
     static void main() throws Exception{
 
         HttpServer server = HttpServer.create(new InetSocketAddress(4321), 0);
-        server.createContext("");
+//        server.createContext("");
 
         server.setExecutor(null);
         server.start();

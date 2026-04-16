7ed7df06c1d04c6f9677291cd0e684c1ddff7bdd
gustavogogola91 <gustavogogola3@gmail.com>
2026-04-16 16:07:51 -0300

    adicionado metodo e para listar times

--- src/main/java/handlers/TimeHandler.java
+++ src/main/java/handlers/TimeHandler.java
@@ -8,11 +8,13 @@ import repository.TimeRepository;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.List;
 
 public class TimeHandler implements HttpHandler {
 
-    private ObjectMapper _mapper = new ObjectMapper();
-    private TimeRepository _repository = new TimeRepository();
+    private final ObjectMapper _mapper = new ObjectMapper();
+    private final TimeRepository _repository = new TimeRepository();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -21,44 +23,49 @@ public class TimeHandler implements HttpHandler {
         String caminho = exchange.getRequestURI().getPath();
         String[] partesCaminho = caminho.split("/");
 
-        switch (metodo) {
-            case "GET":
-
-                if (partesCaminho.length == 2) {
-                    buscarTimePorId(exchange);
-                } else {
-                    listarTimes(exchange);
+        try {
+            switch (metodo) {
+                case "GET" -> {
+                    if (partesCaminho.length == 3) {
+                        buscarTimePorId(exchange, partesCaminho[1]);
+                    } else {
+                        System.out.println("Buscando times");
+                        listarTimes(exchange);
+                    }
                 }
-
-                break;
-
-            case "POST":
-                adicionatTime(exchange);
-                break;
-
-            case "PUT":
-                alterarTime(exchange);
-                break;
-
-            case "DELETE":
-                if( partesCaminho.length == 2) {
-                    deletarTime(exchange, partesCaminho[1]);
-                } else {
-                    exchange.sendResponseHeaders(400, -1);
+                case "POST" -> adicionatTime(exchange);
+                case "PUT" -> alterarTime(exchange);
+                case "DELETE" -> {
+                    if (partesCaminho.length == 3) {
+                        deletarTime(exchange, partesCaminho[1]);
+                    } else {
+                        exchange.sendResponseHeaders(400, -1);
+                    }
                 }
-                break;
+                default -> exchange.sendResponseHeaders(405, -1);
+            }
 
-            default:
-                exchange.sendResponseHeaders(405, -1);
-                break;
+        } catch(Exception e) {
+            String mensagem = "Ocorreu um erro no servidor. " + e.getMessage();
+            exchange.sendResponseHeaders(500, mensagem.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(mensagem.getBytes());
+
+            os.close();
+
         }
     }
 
-    private void listarTimes(HttpExchange exchange) {
+    private void listarTimes(HttpExchange exchange) throws SQLException, IOException {
+        List<Time> times = _repository.listarTimes();
 
+        String resposta = _mapper.writeValueAsString(times);
+
+        enviarResposta(exchange, resposta);
     }
 
-    private void buscarTimePorId(HttpExchange exchange) {
+    private void buscarTimePorId(HttpExchange exchange, String id) {
 
     }
 
--- src/main/java/repository/TimeRepository.java
+++ src/main/java/repository/TimeRepository.java
@@ -1,4 +1,35 @@
 package repository;
 
+import data.ConnectionFactory;
+import models.Time;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeRepository {
+
+    public List<Time> listarTimes() throws SQLException {
+        List<Time> times = new ArrayList<>();
+
+        String sql = "SELECT * FROM tb_time";
+
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Time t = new Time(
+                    rs.getInt("id"),
+                    rs.getString("nome")
+                );
+
+                times.add(t);
+            }
+        }
+        return times;
+    }
 }

775feb75d4d26db10683af8ebd6977956d50cb5c
Vine Moraes <vinemoraes@outlook.com>
2026-04-17 15:21:08 -0300

    adiciona conteudo dos arquivos JogadorHandler e JogadorRepository

---
+++ src/main/java/handlers/JogadorHandler.java
@@ -0,0 +1,167 @@
+package handlers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import exceptions.NotFoundException;
+import models.Jogador;
+import repository.JogadorRepository;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.List;
+
+public class JogadorHandler implements HttpHandler {
+
+    private final ObjectMapper _mapper = new ObjectMapper();
+    private final JogadorRepository _repository = new JogadorRepository();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        String metodo = exchange.getRequestMethod();
+        String caminho = exchange.getRequestURI().getPath();
+        String[] partesCaminho = caminho.split("/");
+
+        try {
+            switch (metodo) {
+                case "GET" -> {
+                    if (partesCaminho.length == 3) {
+                        buscarJogadorPorId(exchange, partesCaminho[2]);
+                    } else {
+                        listarJogadores(exchange);
+                    }
+                }
+                case "POST" -> adicionarJogador(exchange);
+                case "PUT" -> {
+                    if (partesCaminho.length == 3) {
+                        alterarJogador(exchange, partesCaminho[2]);
+                    } else {
+                        exchange.sendResponseHeaders(400, -1);
+                    }
+                }
+                case "DELETE" -> {
+                    if (partesCaminho.length == 3) {
+                        deletarJogador(exchange, partesCaminho[1]);
+                    } else {
+                        exchange.sendResponseHeaders(400, -1);
+                    }
+                }
+                default -> exchange.sendResponseHeaders(405, -1);
+            }
+
+        } catch (NotFoundException e) {
+            String mensagem = "Nenhum item encontrado. ";
+            exchange.sendResponseHeaders(404, mensagem.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(mensagem.getBytes());
+
+            os.close();
+
+        } catch (Exception e) {
+            String mensagem = "Ocorreu um erro no servidor. " + e.getMessage();
+            exchange.sendResponseHeaders(500, mensagem.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(mensagem.getBytes());
+
+            os.close();
+        }
+
+    }
+
+    private void listarJogadores(HttpExchange exchange) throws SQLException, IOException, NotFoundException {
+        List<Jogador> jogadores = _repository.listarJogadores();
+
+        String resposta = _mapper.writeValueAsString(jogadores);
+
+        enviarResposta(exchange, resposta);
+    }
+
+     private void buscarJogadorPorId(HttpExchange exchange, String id)
+            throws SQLException, IOException, NotFoundException {
+        Jogador jogador = _repository.buscarJogadorPorId(id);
+ 
+        String resposta = _mapper.writeValueAsString(jogador);
+ 
+        enviarResposta(exchange, resposta);
+    }
+
+    private void adicionarJogador(HttpExchange exchange) throws IOException, SQLException {
+        Jogador novoJogador = _mapper.readValue(exchange.getRequestBody(), Jogador.class);
+
+        if (novoJogador == null || novoJogador.getNome().isBlank()) {
+            String mensagem = "Erro no objeto enviado";
+            exchange.sendResponseHeaders(400, mensagem.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(mensagem.getBytes());
+
+            os.close();
+
+            return;
+        }
+
+        _repository.adicionarJogador(novoJogador);
+
+        String resposta = "Jogador " + novoJogador.getNome() + " adicionado com sucesso";
+
+        enviarResposta(exchange, resposta);
+    }
+
+    private void alterarJogador(HttpExchange exchange, String id)
+            throws SQLException, IOException, NotFoundException {
+        Jogador jogadorAlterado = _mapper.readValue(exchange.getRequestBody(), Jogador.class);
+ 
+        if (jogadorAlterado == null || jogadorAlterado.getNome().isBlank()) {
+            String mensagem = "Erro no objeto enviado";
+ 
+            exchange.sendResponseHeaders(400, mensagem.getBytes().length);
+ 
+            OutputStream os = exchange.getResponseBody();
+            os.write(mensagem.getBytes());
+ 
+            os.close();
+ 
+            return;
+        }
+ 
+        int linhasAfetadas = _repository.alterarJogador(jogadorAlterado, id);
+ 
+        if (linhasAfetadas == 0) {
+            throw new NotFoundException();
+        }
+ 
+        String resposta = "Jogador alterado com sucesso";
+ 
+        enviarResposta(exchange, resposta);
+    }
+
+    private void deletarJogador(HttpExchange exchange, String id)
+            throws SQLException, IOException, NotFoundException {
+ 
+        int linhasAfetadas = _repository.deletarJogador(id);
+ 
+        if (linhasAfetadas == 0) {
+            throw new NotFoundException();
+        }
+ 
+        String resposta = "Jogador deletado com sucesso";
+ 
+        enviarResposta(exchange, resposta);
+    }
+
+    private void enviarResposta(HttpExchange exchange, String resposta) throws IOException {
+ 
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, resposta.getBytes().length);
+ 
+        OutputStream os = exchange.getResponseBody();
+        os.write(resposta.getBytes());
+ 
+        os.close();
+    }
+}
---
+++ src/main/java/repository/JogadorRepository.java
@@ -0,0 +1,99 @@
+package repository;
+
+import data.ConnectionFactory;
+import exceptions.NotFoundException;
+import models.Jogador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JogadorRepository {
+
+    public List<Jogador> listarJogadores() throws SQLException {
+        List<Jogador> jogadores = new ArrayList<>();
+
+        String sql = "SELECT * FROM tb_jogador";
+
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Jogador j = new Jogador(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getInt("time_id"));
+                jogadores.add(j);
+            }
+        }
+        return jogadores;
+    }
+
+    public Jogador buscarJogadorPorId(String id) throws SQLException, NotFoundException {
+ 
+        Jogador jogador;
+ 
+        String sql = "SELECT * FROM tb_jogador as a WHERE a.id = (?)";
+ 
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+ 
+            int idInt = Integer.parseInt(id);
+ 
+            stmt.setInt(1, idInt);
+ 
+            ResultSet rs = stmt.executeQuery();
+ 
+            if (!rs.next()) {
+                throw new NotFoundException();
+            }
+ 
+            jogador = new Jogador(
+                rs.getInt("id"),
+                rs.getString("nome"),
+                rs.getInt("time_id")
+            );
+        }
+ 
+        return jogador;
+    }
+
+    public void adicionarJogador(Jogador jogador) throws SQLException {
+        String sql = "INSERT INTO tb_jogador (nome, time_id) VALUES (?, ?)";
+ 
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, jogador.getNome());
+            stmt.setInt(2, jogador.getTimeId());
+            stmt.executeUpdate();
+        }
+    }
+
+    public int alterarJogador(Jogador jogador, String id) throws SQLException {
+        String sql = "UPDATE tb_jogador SET nome = ?, time_id = ? WHERE id = ?";
+        int idInt = Integer.parseInt(id);
+ 
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, jogador.getNome());
+            stmt.setInt(2, jogador.getTimeId());
+            stmt.setInt(3, idInt);
+            return stmt.executeUpdate();
+        }
+    }
+
+    public int deletarJogador(String id) throws SQLException {
+        String sql = "DELETE FROM tb_jogador WHERE id = ?";
+        int idInt = Integer.parseInt(id);
+ 
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, idInt);
+            return stmt.executeUpdate();
+        }
+    }
+}
